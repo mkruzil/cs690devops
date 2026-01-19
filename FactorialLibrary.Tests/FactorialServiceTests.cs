df7367c534b4966f2fe611352f63e30dcfff2a34
Michael Kruzil <1523573+mkruzil@users.noreply.github.com>
2026-01-19 03:05:49 +0000

    Changes

---
+++ FactorialLibrary.Tests/FactorialServiceTests.cs
@@ -0,0 +1,12 @@
+﻿namespace FactorialService.Tests;
+using FactorialLibrary;
+public class FactorialServiceTests
+{
+  [Fact]
+  public void Test1()
+  {
+    var result = FactorialService.Factorial(0);
+    Assert.Equal(1,result);
+  }
+}
+
